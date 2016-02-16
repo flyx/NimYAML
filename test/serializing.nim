9be97ff38696ba8705538c08b0a39c075aea4ed3
Felix Krause <contact@flyx.org>
2016-02-16 19:24:55 +0100

    Added constructChild for better serialization.
     - Handle tag and anchor checks and ref types in constructChild
     - Added tests for using custom representObject and constructObject
       procs
     - Code cleanup

--- test/serializing.nim
+++ test/serializing.nim
@@ -1,5 +1,5 @@
 import "../yaml"
-import unittest
+import unittest, strutils
 
 type
     MyTuple = tuple
@@ -18,9 +18,30 @@ type
     Node = object
         value: string
         next: ref Node
+    
+    BetterInt = int
 
 setTagUriForType(TrafficLight, "!tl")
 setTagUriForType(Node, "!example.net:Node")
+setTagUriForType(BetterInt, "!test:BetterInt")
+
+proc representObject*(value: BetterInt, ts: TagStyle = tsNone,
+                      c: SerializationContext): RawYamlStream {.raises: [].} =
+    result = iterator(): YamlStreamEvent =
+        var
+            val = $value
+            i = val.len - 3
+        while i > 0:
+            val.insert("_", i)
+            i -= 3
+        yield scalarEvent(val, presentTag(BetterInt, ts), yAnchorNone)
+
+proc constructObject*(s: var YamlStream, c: ConstructionContext,
+                      result: var BetterInt)
+        {.raises: [YamlConstructionError, YamlStreamError].} =
+    var item: YamlStreamEvent
+    constructScalarItem(s, item, "BetterInt"):
+        result = BetterInt(parseBiggestInt(item.scalarContent) + 1)
 
 template assertStringEqual(expected, actual: string) =
     for i in countup(0, min(expected.len, actual.len)):
@@ -40,11 +61,8 @@ suite "Serialization":
 
     test "Serialization: Load string sequence":
         let input = newStringStream(" - a\n - b")
-        var
-            result: seq[string]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: seq[string]
+        load(input, result)
         assert result.len == 2
         assert result[0] == "a"
         assert result[1] == "b"
@@ -57,11 +75,8 @@ suite "Serialization":
     
     test "Serialization: Load Table[int, string]":
         let input = newStringStream("23: dreiundzwanzig\n42: zweiundvierzig")
-        var
-            result: Table[int32, string]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: Table[int32, string]
+        load(input, result)
         assert result.len == 2
         assert result[23] == "dreiundzwanzig"
         assert result[42] == "zweiundvierzig"
@@ -78,11 +93,8 @@ suite "Serialization":
     
     test "Serialization: Load Sequences in Sequence":
         let input = newStringStream(" - [1, 2, 3]\n - [4, 5]\n - [6]")
-        var
-            result: seq[seq[int32]]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: seq[seq[int32]]
+        load(input, result)
         assert result.len == 3
         assert result[0] == @[1.int32, 2.int32, 3.int32]
         assert result[1] == @[4.int32, 5.int32]
@@ -98,11 +110,8 @@ suite "Serialization":
     
     test "Serialization: Load Enum":
         let input = newStringStream("!nim:system:seq(tl)\n- !tl tlRed\n- tlGreen\n- tlYellow")
-        var
-            result: seq[TrafficLight]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: seq[TrafficLight]
+        load(input, result)
         assert result.len == 3
         assert result[0] == tlRed
         assert result[1] == tlGreen
@@ -117,11 +126,8 @@ suite "Serialization":
     
     test "Serialization: Load Tuple":
         let input = newStringStream("str: value\ni: 42\nb: true")
-        var
-            result: MyTuple
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: MyTuple
+        load(input, result)
         assert result.str == "value"
         assert result.i == 42
         assert result.b == true
@@ -135,11 +141,8 @@ suite "Serialization":
     
     test "Serialization: Load custom object":
         let input = newStringStream("firstnamechar: P\nsurname: Pan\nage: 12")
-        var
-            result: Person
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: Person
+        load(input, result)
         assert result.firstnamechar == 'P'
         assert result.surname   == "Pan"
         assert result.age == 12
@@ -155,11 +158,8 @@ suite "Serialization":
     test "Serialization: Load sequence with explicit tags":
         let input = newStringStream("--- !nim:system:seq(" &
                 "tag:yaml.org,2002:str)\n- !!str one\n- !!str two")
-        var
-            result: seq[string]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: seq[string]
+        load(input, result)
         assert result[0] == "one"
         assert result[1] == "two"
     
@@ -174,11 +174,8 @@ suite "Serialization":
     test "Serialization: Load custom object with explicit root tag":
         let input = newStringStream(
             "--- !nim:custom:Person\nfirstnamechar: P\nsurname: Pan\nage: 12")
-        var
-            result: Person
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        construct(events, result)
+        var result: Person
+        load(input, result)
         assert result.firstnamechar == 'P'
         assert result.surname   == "Pan"
         assert result.age       == 12
@@ -223,17 +220,12 @@ next:
 - *b
 - *c
 """)
-        var
-            result: seq[ref Node]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        try:
-            construct(events, result)
+        var result: seq[ref Node]
+        try: load(input, result)
         except YamlConstructionError:
             let ex = (ref YamlConstructionError)(getCurrentException())
-            echo "line ", parser.getLineNumber, ", column ",
-                parser.getColNumber, ": ", ex.msg
-            echo parser.getLineContent
+            echo "line ", ex.line, ", column ", ex.column, ": ", ex.msg
+            echo ex.lineContent
             raise ex
 
         assert(result.len == 3)
@@ -246,17 +238,12 @@ next:
     
     test "Serialization: Load nil values":
         let input = newStringStream("- ~\n- !!str ~")
-        var
-            result: seq[ref string]
-            parser = newYamlParser(tagLib)
-            events = parser.parse(input)
-        try:
-            construct(events, result)
+        var result: seq[ref string]
+        try: load(input, result)
         except YamlConstructionError:
             let ex = (ref YamlConstructionError)(getCurrentException())
-            echo "line ", parser.getLineNumber, ", column ",
-                parser.getColNumber, ": ", ex.msg
-            echo parser.getLineContent
+            echo "line ", ex.line, ", column ", ex.column, ": ", ex.msg
+            echo ex.lineContent
             raise ex
         
         assert(result.len == 2)
@@ -272,4 +259,21 @@ next:
         dump(input, output, psBlockOnly, tsRootOnly)
         assertStringEqual "%YAML 1.2\n--- !nim:system:seq(tag:yaml.org,2002:str) \n- !!null ~\n- !!str ~",
                 output.data
-        
+    
+    test "Serialization: Custom constructObject":
+        let input = newStringStream("- 1\n- !test:BetterInt 2")
+        var result: seq[BetterInt]
+        load(input, result)
+        assert(result.len == 2)
+        assert(result[0] == 2)
+        assert(result[1] == 3)
+    
+    test "Serialization: Custom representObject":
+        let input = @[1.BetterInt, 9998887.BetterInt, 98312.BetterInt]
+        var output = newStringStream()
+        dump(input, output, psBlockOnly, tsAll)
+        assertStringEqual """%YAML 1.2
+--- !nim:system:seq(test:BetterInt) 
+- !test:BetterInt 1
+- !test:BetterInt 9_998_887
+- !test:BetterInt 98_312""", output.data
