9de9fcd50b144a40c214d4c89cdeac0393b6fc36
quantimnot <quantimnot@users.noreply.github.com>
2021-10-21 22:09:21 -0400

    Change serialization to omit `none(T)` fields of sparse objects

--- yaml/serialization.nim
+++ yaml/serialization.nim
@@ -967,12 +967,18 @@ macro genRepresentObject(t: typedesc, value, childTagStyle: typed) =
         name = $child
         childAccessor = newDotExpr(value, newIdentNode(name))
       result.add(quote do:
-        when not `childAccessor`.hasCustomPragma(transient):
+        template representChild =
           when bool(`isVO`): c.put(startMapEvent())
           c.put(scalarEvent(`name`, if `childTagStyle` == tsNone:
               yTagQuestionMark else: yTagNimField, yAnchorNone))
           representChild(`childAccessor`, `childTagStyle`, c)
           when bool(`isVO`): c.put(endMapEvent())
+        when not `childAccessor`.hasCustomPragma(transient):
+          when not hasSparse(`t`) or (hasSparse(`t`) and `child` is not Option):
+            representChild()
+          elif hasSparse(`t`) and `child` is Option:
+            if `childAccessor`.isSome:
+              representChild()
       )
     inc(fieldIndex)
 
